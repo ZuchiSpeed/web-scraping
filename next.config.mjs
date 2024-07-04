75bd03f8cff806221aa6f63b0442f45fbc99855a
ZuchiSpeed <zuchi737@gmail.com>
2024-07-04 05:50:17 +0200

    navbar ui component

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        serverActions: true,
-        serverComponentsExternalPackages: ['mongoose']
-    },
-    images: {
-        domains: ['m.media-amazon.com']
-    }
-}
+const nextConfig = {}
 
-module.exports = nextConfig
+export default nextConfig
