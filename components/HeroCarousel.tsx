6ee955c7173e138cdaf637f35c869275b3c325e4
ZuchiSpeed <zuchi737@gmail.com>
2024-07-04 18:20:33 +0200

    added searchBar UI & Image Carousel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+'use client'
 
-export default function HeroCarousel() {
+import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const heroImages = [
+  { imgUrl: "/assets/images/hero-1.svg", alt: "smartwatch" },
+  { imgUrl: "/assets/images/hero-2.svg", alt: "bag" },
+  { imgUrl: "/assets/images/hero-3.svg", alt: "lamp" },
+  { imgUrl: "/assets/images/hero-4.svg", alt: "air fryer" },
+  { imgUrl: "/assets/images/hero-5.svg", alt: "chair" },
+];
+
+const HeroCarousel = () => {
   return (
-    <div>HeroCarousel</div>
-  )
-}
+    <div className="hero-carousel">
+      <Carousel
+        showThumbs={false}
+        //autoPlay
+        infiniteLoop
+        //interval={2000}
+        showArrows={false}
+        showStatus={false}
+      >
+        {heroImages.map((image, index) => (
+          <div key={index}>
+            <Image
+              src={image.imgUrl}
+              alt={image.alt}
+              width={484}
+              height={484}
+              className="object-contain"
+            />
+          </div>
+        ))}
+      </Carousel>
+
+      <Image 
+      src="assets/icons/hand-drawn-arrow.svg"
+      alt="arrow"
+      width={175}
+      height={175}
+      className="max-xl:hidden absolute -left-[15%] bottom-0"
+      />
+    </div>
+  );
+};
+
+export default HeroCarousel;
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+"use client";
 
-export default function Searchbar() {
+import React from "react";
+
+const Searchbar = () => {
+  const handleSubmit = () => {};
+
   return (
-    <div>Searchbar</div>
-  )
-}
+    <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-12">
+
+      <input
+        type="text"
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+
+      <button type="submit" className="searchbar-btn">Search</button>
+    </form>
+  );
+};
+
+export default Searchbar;
